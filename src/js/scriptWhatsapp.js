03732b83c8132b14dbcbe1629d07194bd7c8881f
Danilo Oliveira <danilooliveira@MacBook-Pro-de-Danilo.local>
2020-02-05 14:25:58 -0300

    finished

--- src/js/scriptWhatsapp.js
+++ src/js/scriptWhatsapp.js
@@ -209,10 +209,61 @@ function maria() {
   const body = document.querySelector('#tutorial');
   body.classList.remove("findmaria");
   body.classList.add("maria");
+
+  removeInputText();
+  
+  const styleInputSendText = {
+    bottom: '20px',
+    left: '20px',
+    width: '80%',
+    height: '130px',
+    boxSizing: 'border-box',
+    borderRadius: '100px' 
+  }
+  inputSendText(styleInputSendText);
+
+  const boxMessage1 = {
+    text: 'vamos mandar uma mensagem para maria. cliqie no campo mensagem sinalizado.',
+    article: {
+      style:{
+        width: '70%',
+        padding: '80px 40px'
+      }
+    }
+  }
+  boxMessage(boxMessage1);
+
+  const styleSinalizar = {
+    bottom: '5px',
+    left: '130px',
+    width: '470px',
+    height: '163px',
+    boxSizing: 'border-box'
+  }
+  sinalizar(styleSinalizar, helloMaria);
+}
+
+function helloMaria() {
+  focusInputSendText();
+  const boxMessage1 = {
+    text: 'agora escreva uma mensagem. exemplo: "olá, tudo bem?"',
+    article: {
+      style:{
+        width: '70%',
+        padding: '80px 40px'
+      }
+    }
+  }
+  boxMessage(boxMessage1);
 }
 
 function focusInput() {
   const input = document.querySelector('#inputText');
+  input.focus();
+}
+
+function focusInputSendText() {
+  const input = document.querySelector('#inputSendText');
   input.focus();
 }
 
@@ -244,7 +295,7 @@ function sinalizar(style, event) {
 function inputTextFind(nome, style, callback) {
   const body = document.querySelector('#tutorial');
   const temp = document.querySelector('#inputText');
-  if(!!temp) temp.remove();
+  if(!!temp) removeInputText();
 
   let inputText = document.createElement("input");
   inputText.setAttribute('id','inputText');  
@@ -271,6 +322,7 @@ function inputTextFind(nome, style, callback) {
   })
   
   let iconBack = document.createElement("img");
+  iconBack.setAttribute('id', 'ico-back');
   iconBack.setAttribute('src','./src/ico/svg/arrow_back.svg');
   let style2 = {
     position: 'absolute',
@@ -280,6 +332,7 @@ function inputTextFind(nome, style, callback) {
   Object.assign(iconBack.style,style2);
 
   let iconClose = document.createElement("img");
+  iconClose.setAttribute('id', 'ico-close');
   iconClose.setAttribute('src','./src/ico/svg/close.svg');
   let style3 = {
     position: 'absolute',
@@ -292,6 +345,136 @@ function inputTextFind(nome, style, callback) {
   body.appendChild(iconBack);
   body.appendChild(iconClose);
 
+}
+
+function removeInputText() {
+  const input = document.querySelector('#inputText');
+  const icon1 = document.querySelector('#ico-close');
+  const icon2 = document.querySelector('#ico-back');
+  input.remove();
+  icon1.remove();
+  icon2.remove();
+}
+
+function inputSendText(style, callback) {
+  const body = document.querySelector('#tutorial');
+  const temp = document.querySelector('#inputSendText');
+  if(!!temp) {
+    const back = document.querySelector('#back-ico');
+    const close = document.querySelector('#close-ico');
+    temp.remove();
+    back.remove();
+    close.remove();
+  }
+
+  let inputText = document.createElement("input");
+  inputText.setAttribute('id','inputSendText');  
+  inputText.setAttribute('type','text');
+  inputText.setAttribute('contentEditable','true');
+  style.position = 'absolute';
+  style.boxSizing = 'border-box';
+  style.padding = '0px 120px';
+  style.fontSize = '50px';
+  Object.assign(inputText.style,style);
+  
+  let iconBack = document.createElement("img");
+  iconBack.setAttribute('src','./src/ico/svg/tag_faces.svg');
+  let style2 = {
+    position: 'absolute',
+    left: '58px', bottom: '51px',
+    zIndex: 0, width: '69px'
+  };
+  Object.assign(iconBack.style,style2);
+
+  let iconAttach = document.createElement("img");
+  iconAttach.setAttribute('src','./src/ico/svg/attach_file.svg');
+  let style3 = {
+    position: 'absolute',
+    right: '312px', bottom: '49px',
+    zIndex: 0, width: '69px', transform: 'rotateZ(-35deg)'
+  };
+  Object.assign(iconAttach.style,style3);
+  
+  let iconCamera = document.createElement("img");
+  iconCamera.setAttribute('src','./src/ico/svg/camera_alt.svg');
+  let style4 = {
+    position: 'absolute',
+    right: '205px', bottom: '49px',
+    zIndex: 0, width: '69px'
+  };
+  Object.assign(iconCamera.style,style4);
+
+
+  var iconSend = document.createElement("img");
+  iconSend.setAttribute('src','./src/ico/svg/mic.svg');
+  let divText = document.createElement("div");
+  divText.classList.add("btn-send");
+  divText.appendChild(iconSend);
+  body.appendChild(divText);
+
+  inputText.addEventListener('keyup', (e)=>{
+    let text = e.target.value;
+
+    if(text.length > 0) {
+      iconCamera.style.opacity = 0;
+      iconAttach.style.right = '212px';
+      iconSend.removeAttribute('src');
+      iconSend.setAttribute('src','./src/ico/svg/send.svg');
+    } else if(text.length == 0) {
+      iconCamera.style.opacity = 1;
+      iconAttach.style.right = '312px';
+      iconSend.removeAttribute('src');
+      iconSend.setAttribute('src','./src/ico/svg/mic.svg');
+    }
+  });
+
+  function message(text) {
+    const body = document.querySelector('#tutorial');
+    let article = document.createElement('article');
+    let sub = document.createElement('sub');
+    sub.innerHTML = getHours();
+    sub.style.fontSize = '30px';
+    sub.style.padding = '0 20px';
+    article.innerHTML = text;
+    article.classList.add('box-sendMsg');
+    article.appendChild(sub);
+    body.appendChild(article);
+  }
+
+  iconSend.addEventListener('click', (e)=>{
+    let text = inputText.value;
+    
+    if(text.length > 0) {
+      inputText.value = '';
+      iconCamera.style.opacity = 1;
+      iconAttach.style.right = '312px';
+      iconSend.removeAttribute('src');
+      iconSend.setAttribute('src','./src/ico/svg/mic.svg');
+      inputText.setAttribute('disabled', 'true');
+      message(text);
+    }
+  
+  });
+
+  body.appendChild(inputText);
+  body.appendChild(iconBack);
+  body.appendChild(iconAttach);
+  body.appendChild(iconCamera);
+
+}
+
+function addZero(i) {
+  if (i < 10) {
+    i = "0" + i;
+  }
+  return i;
+}
+
+function getHours() {
+  var d = new Date();
+  var h = addZero(d.getHours());
+  var m = addZero(d.getMinutes());
+  return h + ":" + m ;
 }
 
 function btnWhatsapp() {
